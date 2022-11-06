382742c76348a71f78efcbff2077a5852de6c761
steph ridnell <steph.ridnell@gmail.com>
2022-11-06 15:18:22 +1100

    Added login error handling

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -9,9 +9,18 @@
           <form class="w-100" @submit="login">
             <div class="p-12">
               <input type="text" v-model="email" placeholder="email" />
+              <div class="form-error" v-if="emailError">
+                {{ emailError }}
+              </div>
             </div>
             <div class="p-12">
               <input type="password" v-model="password" placeholder="password" />
+              <div class="form-error">
+                {{ passwordError }}
+              </div>
+          </div>
+            <div class="form-error mt-0" v-if="formError">
+              {{ formError }}
             </div>
             <div class="d-flex ai-center jc-sb p-16">
               <button class="button text-bold" type="submit">
@@ -39,18 +48,36 @@ export default {
   data: () => {
     return {
       email: '',
-      password: ''
+      password: '',
+      emailError: '',
+      passwordError: '',
+      formError: ''
     }
   },
   methods: {
+    resetErrors () {
+      this.emailError = ''
+      this.passwordError = ''
+      this.formError = ''
+    },
     async login (e) {
       e.preventDefault()
+      this.resetErrors()
+      if (!this.email) {
+        this.emailError = 'Email is required'
+      }
+      if (!this.password) {
+        this.passwordError = 'Password is required'
+      }
+      if (this.passwordError || this.emailError) {
+        return
+      }
       try {
         const data = await http.post('/auth/login', { email: this.email, password: this.password })
         this.$store.commit('setCurrentUser', data.user)
         this.$router.push('/')
       } catch (e) {
-        console.log(e.message)
+        this.formError = e.msg
       }
     }
   }
